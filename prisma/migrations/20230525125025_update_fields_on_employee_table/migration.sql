f6ae2ba76442d8d4cced06ec5a851e5bc2f7961e
Samuel Pereira <gmr.quest.10@gmail.com>
2023-05-25 09:51:37 -0300

    chore: update fields on employee table

---
+++ prisma/migrations/20230525125025_update_fields_on_employee_table/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `name` on the `Employee` table. All the data in the column will be lost.
+  - Added the required column `fullName` to the `Employee` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `password` to the `Employee` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Employee" DROP COLUMN "name",
+ADD COLUMN     "fullName" VARCHAR(64) NOT NULL,
+ADD COLUMN     "password" VARCHAR(64) NOT NULL;
