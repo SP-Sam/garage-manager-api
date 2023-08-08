9c1b24bd3a21d50e31b6d44914068b5ea183bc75
Samuel Pereira <gmr.quest.10@gmail.com>
2023-08-08 14:24:32 -0300

    chore: add @nestjs/config lib

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,9 +8,11 @@ import { CustomersModule } from './modules/customers/customer.module';
 import { ServicesModule } from './modules/services/services.module';
 import { CarPartsModule } from './modules/car-parts/car-parts.module';
 import { VehiclesModule } from './modules/vehicles/vehicles.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     AuthModule,
     RolesModule,
     EmployeesModule,
