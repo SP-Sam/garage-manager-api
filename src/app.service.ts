a0b21ce0d01985b99cb2b8857c27a777303aa36e
Samuel Pereira <gmr.quest.10@gmail.com>
2023-08-08 14:30:50 -0300

    chore: add healthcheck message to env vars

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello() {
-    return { message: 'Garage Manager API - v1.0.0' };
+    return { message: process.env.HEALTHCHECK_MESSAGE };
   }
 }
